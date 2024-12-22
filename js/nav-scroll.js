22384a4392155f2b01693045f0dcedcd987717fb
odoi21 <solotecladosinl2@gmail.com>
2024-12-22 00:41:35 -0300

    delete

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -11,18 +11,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// Detectar scroll
-window.addEventListener('scroll', handleNavbarScroll);
-
-// Detectar cuando se abre el menú desplegable
-navCollapse.addEventListener('show.bs.collapse', () => {
-    navbar.classList.add('navbar-open'); // Navbar se vuelve opaco al abrir el menú
-    navbar.classList.remove('navbar');
-});
-
-// Detectar cuando se cierra el menú desplegable
-navCollapse.addEventListener('hide.bs.collapse', () => {
-    navbar.classList.remove('navbar-open');
-    handleNavbarScroll(); // Reaplica el estado dependiendo del scroll
-});
-
